6b009d12799fcdc6b5a48a495c5a25cf82719bfc
ocean3037 <131468615+ocean3037@users.noreply.github.com>
2024-04-19 10:49:56 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -6,7 +6,7 @@ sever = os.environ["SERVE"]
 sckey = os.environ["SCKEY"]
 #'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42796bb9'
 # 填入glados账号对应cookie
-cookie = os.environ["COOKIE"]
+cookie = os.environ["koa:sess=eyJ1c2VySWQiOjM3OTE4MywiX2V4cGlyZSI6MTcyNjU1NTI3NTU2MCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=PhMhUd7UnB4W7sxzu1s1trDscOA; _gid=GA1.2.323323087.1713493174; _gat_gtag_UA_104464600_2=1; _ga=GA1.1.1596524711.1694177878; _ga_CZFVKMNT9J=GS1.1.1713493174.9.1.1713493834.0.0.0"]
 #'__cfduid=d3459ec306384ca67a65170f8e2a5bd561593049467; _ga=GA1.2.766373509.1593049472; _gid=GA1.2.1338236108.1593049472; koa:sess=eyJ1c2VySWQiOjQxODMwLCJfZXhwaXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=6qG8SyMh_5KpSB6LBc9yRviaPvI'
 
 
